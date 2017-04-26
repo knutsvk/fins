0db250db515c8cba1353062872627dafc7700c4f
knutsvk <ksk38@cam.ac.uk>
2017-04-26 12:02:01 +0100

    moved all settings into separate file, fixed norm computations to check values throughout domain

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
 import fenics
-import rheology
 import pde
+import rheology
+from settings import *
 import testcases
 
 fenics.set_log_level(fenics.ERROR)
 
-rho = 1.0
-mu = 1.0
-tau_y = 0.3
-eps = 1e-4
-fluid = rheology.Sigmoid(rho, mu, tau_y, eps)
-
-t = 0.0
-dt = 0.05
-t_end = 1.0
-
-nx = ny = 32
+fluid = rheology.Sigmoid(rho, mu, ty, eps)
 problem = testcases.Poiseuille(nx, ny)
-
 u, u0 = problem.define_functions()
 algorithm = pde.Abali(problem, dt, fluid, u, u0)
 
@@ -25,15 +15,23 @@ outdir = './output/'
 file_p = fenics.File(outdir + problem.name + '_' + fluid.name + '_' + 'pressure.pvd')
 file_v = fenics.File(outdir + problem.name + '_' + fluid.name + '_' + 'velocity.pvd')
 
+t = 0.0
 step = 0
-print('step\t time\t umax\t change')
-while t < t_end:
+change = 1
+print('step\t time\t vmax\t change')
+
+while t < t_end and change > sstol:
     step += 1
     t += dt
     algorithm.advance(u)
 
-    file_p << (u.split()[0], t)
-    file_v << (u.split()[1], t)
+    pressure, velocity = u.split(deepcopy=True)
 
-    print('%d\t %.3f\t %.3f\t %.2e' % (step, t, u(0.5, 0.5)[1], u(0.5, 0.5)[1] - u0(0.5, 0.5)[1]))
+    file_p << (pressure, t)
+    file_v << (velocity, t)
+
+    vmax = velocity.vector().norm('linf')
+    change = (velocity.vector()-u0.split(deepcopy=True)[1].vector()).norm('linf')
+    print('%d\t %.3f\t %.3f\t %.2e' % (step, t, vmax, change))
+
     u0.assign(u)
--- settings.py
+++ settings.py
@@ -1,2 +1,17 @@
-reltol = 1e-5
-maxiter = 100
+# Fluid properties
+rho = 1.0       # Density
+mu  = 1.0       # Dynamic viscosity coefficient
+ty  = 0.3       # Yield stess
+eps = 1e-4      # Regularisation parameter
+
+# Spatial discretisation
+nx = ny = 32    # Number of elements per direction
+
+# Settings for time loop
+dt    = 0.05    # Time step size
+t_end = 1.0     # Final time
+sstol = 1e-5    # Steady-state tolerance
+
+# Nonlinear solver settings
+reltol = 1e-5   # Relative tolerance required for convergence
+maxiter = 100   # Maximum allowed iterations per solve
