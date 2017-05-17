6452823cd1c53f1c304246d677ca84d9e7f53bae
knutsvk <ksk38@cam.ac.uk>
2017-05-17 20:16:51 +0100

    solver is now linear since problem is linear

--- stokes/pde.py
+++ stokes/pde.py
@@ -1,8 +1,8 @@
 """
-Incompressible Navier-Stokes equations
+Generalized Stokes Problem
 
-  rho(u' + u . nabla(u)) - div(sigma(u, p)) = f
-                                 div(u) = 0
+  alpha u + nu lap(u) - grad(p) = f
+                         div(u) = 0
 """
 
 from fenics import *
@@ -11,53 +11,34 @@ class GSP():
     """
     Generalised Stokes Problem
     """
-    def __init__(self, _testcase, _alpha, _nu, U):
+    def __init__(self, _testcase, _alpha, _nu):
         self.testcase = _testcase
         self.alpha = Constant(_alpha)
         self.nu = Constant(_nu)
-        self.form, self.gain = self.variational_form(U)
+        self.a, self.L = self.variational_form(self.testcase.f)
 
-    def variational_form(self, U):
+    def variational_form(self, f):
         """
-        Define the nonlinear variational problem to be solved
+        Define the linear variational problem to be solved
         """
 
         # Define trial and test functions
-        test = TestFunction(self.testcase.space)
-        q, v = split(test)
-        dU = TrialFunction(self.testcase.space)
-        p, u = split(U)
+        q, v = TestFunctions(self.testcase.space)
+        p, u = TrialFunctions(self.testcase.space)
 
-        # Define expressions used in variational forms
-        facets = FacetFunction('size_t', self.testcase.mesh)
-        cells = CellFunction('size_t', self.testcase.mesh)
-
-        da_ = Measure('ds', domain=self.testcase.mesh, subdomain_data=facets)
-        dv_ = Measure('dx', domain=self.testcase.mesh, subdomain_data=cells)
-
-        # Define form and gain for problem
-        form = (self.alpha * dot(u, v)
+        # Define bilinear and linear forms (resp. a and L)
+        a = (self.alpha * inner(u, v)
                 + self.nu * inner(grad(u), grad(v))
                 - p * div(v)
-                - dot(self.testcase.f, v)
-                + q * div(u)) * dv_
+                + q * div(u)) * dx()
 
-        gain = derivative(form, U, dU)
+        L = inner(f, v) * dx()
 
-        return form, gain
+        return a, L
 
     def solve(self, U):
-        # Advance the system to time t+dt by performing Newton-Raphson solve
-        p, u = U.split(deepcopy=True)
-        goal = div(u)*dx()
-        tol = 1e-12
-
-        problem = NonlinearVariationalProblem(self.form, U, self.testcase.bc, J=self.gain,
-                form_compiler_parameters={"optimize": True, "cpp_optimize": True,
-                    "representation": "quadrature", "quadrature_degree": 2})
-        solver = AdaptiveNonlinearVariationalSolver(problem, goal)
-#        solver.parameters["newton_solver"]["linear_solver"] = "cg"
-#        solver.parameters["newton_solver"]["preconditioner"] = "icc"
-        solver.solve(tol)
-
-        return k
+        """
+        """
+        problem = LinearVariationalProblem(self.a, self.L, U, self.testcase.bc)
+        solver = LinearVariationalSolver(problem)
+        solver.solve()
