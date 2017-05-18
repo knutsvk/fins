848657bcd6d7f0a0c67bd4c8d6d24ac96a77843f
knutsvk <ksk38@cam.ac.uk>
2017-05-18 15:49:09 +0100

    made all testcases subclasses of parent class Problem

--- stokes/testcases.py
+++ stokes/testcases.py
@@ -1,7 +1,58 @@
+from abc import ABC, abstractmethod
 from fenics import * 
 import numpy as np
 
-class Smooth():
+
+class Problem(ABC):
+    """
+    Abstract class which describes the specific problem for which the Stokes flow equations  are
+    solved. The specific problem must have a name and define a mesh, function space(s), boundary
+    conditions and external forces. Optionally, analytical solutions and error computers may be
+    specified. 
+    """
+    def __init__(self, nx, ny, _name):
+        self.mesh = self.make_mesh(nx, ny)
+        self.space = self.make_space()
+        self.bc = self.boundary_conditions()
+        self.f = self.external_forces()
+        self.name = _name
+    
+    @staticmethod
+    @abstractmethod
+    def make_mesh(nx, ny):
+        return UnitSquareMesh(nx, ny)
+
+    @abstractmethod
+    def make_space(self):
+        scalar_element = FiniteElement('Lagrange', triangle, 1)
+        vector_element = VectorElement('Lagrange', triangle, 2)
+        mixed_element = MixedElement([scalar_element, vector_element])
+        return FunctionSpace(self.mesh, mixed_element)
+
+    @abstractmethod
+    def boundary_conditions(self):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def external_forces():
+        return Constant((0, 0))
+
+    def define_functions(self):
+        u = Function(self.space)
+        return u
+
+    def write_file(self, U, nx, ny, outdir='./output/'):
+        p, u = U.split(deepcopy=True)
+        file_p = File(outdir + self.name + '_nx' + str(nx) + '_ny' + str(ny) + '_' +
+                'pressure.pvd')
+        file_u = File(outdir + self.name + '_nx' + str(nx) + '_ny' + str(ny) + '_' +
+                'velocity.pvd')
+        file_p << p
+        file_u << u
+
+
+class Smooth(Problem):
     """
     Smooth test case as described on page 884 in: 
     J. Cahouet and J.-P. Chabard (1988)
@@ -9,21 +60,13 @@ class Smooth():
     International Journal for Numerical Methods in Fluids, vol. 8, 869-895
     """
     def __init__(self, nx, ny):
-        self.mesh = self.make_mesh(nx, ny)
-        self.space = self.make_space()
-        self.bc = self.boundary_conditions()
-        self.ic = self.initial_conditions()
-        self.f = self.external_forces()
-        self.name = 'smooth'
+        super().__init__(nx, ny, 'smooth')
 
-    def make_mesh(self, nx, ny): 
-        return UnitSquareMesh(nx, ny)
+    def make_mesh(self, nx, ny):
+        return super().make_mesh(nx, ny)
 
     def make_space(self):
-        scalar_element = FiniteElement('Lagrange', triangle, 1)
-        vector_element = VectorElement('Lagrange', triangle, 2)
-        mixed_element = MixedElement([scalar_element, vector_element])
-        return FunctionSpace(self.mesh, MixedElement([scalar_element, vector_element]))
+        return super().make_space()
 
     def boundary_conditions(self):
         left = CompiledSubDomain('near(x[0], 0.0) && on_boundary')
@@ -33,11 +76,17 @@ class Smooth():
         bcs = [left, right, bottom, top]
         return [DirichletBC(self.space.sub(1), Constant((0, 0)), bc) for bc in bcs]
 
-    def initial_conditions(self):
-        return Constant((0.0, 0.0, 0.0))
+    @staticmethod
+    def external_forces():
+        return Constant((-1, 1))
 
-    def external_forces(self):
-        return Constant((-1.0, 1.0))
+    @staticmethod
+    def exact_pressure():
+        return Expression('-x[0]+x[1]', degree=1)
+
+    @staticmethod
+    def exact_velocity():
+        return Constant((0, 0))
 
     def compute_errors(self, U):
         p, u = U.split(deepcopy=True)
@@ -56,28 +105,8 @@ class Smooth():
 
         return error_norms_p, error_norms_u
 
-    def exact_pressure(self):
-        """
-        Analytical solution for the pressure in this testcase
-        """
-        return Expression('-x[0]+x[1]', degree=1)
 
-    def exact_velocity(self):
-        """
-        Analytical solution for the velocity in this testcase
-        """
-        return Constant((0.0, 0.0))
-
-    def define_functions(self):
-        """
-        Define function to hold solution 
-        """
-        u = Function(self.space)
-        u = interpolate(self.ic, self.space)
-        return u
-
-
-class PolkaDots():
+class PolkaDots(Problem):
     """
     Analytical test case as described on page 390 in: 
     A. Logg et al. (2012)
@@ -85,21 +114,13 @@ class PolkaDots():
     Springer-Verlag Berlin Heidelberg
     """
     def __init__(self, nx, ny):
-        self.mesh = self.make_mesh(nx, ny)
-        self.space = self.make_space()
-        self.bc = self.boundary_conditions()
-        self.ic = self.initial_conditions()
-        self.f = self.external_forces()
-        self.name = 'polka'
+        super().__init__(nx, ny, 'polka')
 
     def make_mesh(self, nx, ny): 
         return UnitSquareMesh(nx, ny, 'crossed')
 
     def make_space(self):
-        scalar_element = FiniteElement('Lagrange', triangle, 1)
-        vector_element = VectorElement('Lagrange', triangle, 2)
-        mixed_element = MixedElement([scalar_element, vector_element])
-        return FunctionSpace(self.mesh, MixedElement([scalar_element, vector_element]))
+        return super().make_space()
 
     def boundary_conditions(self):
         pin = CompiledSubDomain('near(x[0], 0.0)  && on_boundary')
@@ -117,12 +138,19 @@ class PolkaDots():
 
         return [p_bc, u_bc1, u_bc2, u_bc3, u_bc4]
 
-    def initial_conditions(self):
-        return Constant((0, 0, 0))
-
-    def external_forces(self):
+    @staticmethod
+    def external_forces():
         return Expression(('28*pi*pi * sin(4*pi*x[0]) * cos(4*pi*x[1])', 
             '- 36*pi*pi * cos(4*pi*x[0]) * sin(4*pi*x[1])'), degree=10)
+
+    @staticmethod
+    def exact_pressure():
+        return Expression('pi * cos(4*pi*x[0]) * cos(4*pi*x[1])', degree=10)
+
+    @staticmethod
+    def exact_velocity():
+        return Expression(('sin(4*pi*x[0]) * cos(4*pi*x[1])', 
+            '- cos(4*pi*x[0]) * sin(4*pi*x[1])'), degree=10)
 
     def compute_errors(self, U):
         p, u = U.split(deepcopy=True)
@@ -141,29 +169,8 @@ class PolkaDots():
 
         return error_norms_p, error_norms_u
 
-    def exact_pressure(self):
-        """
-        Analytical solution for the pressure in this testcase
-        """
-        return Expression('pi * cos(4*pi*x[0]) * cos(4*pi*x[1])', degree=10)
 
-    def exact_velocity(self):
-        """
-        Analytical solution for the velocity in this testcase
-        """
-        return Expression(('sin(4*pi*x[0]) * cos(4*pi*x[1])', 
-            '- cos(4*pi*x[0]) * sin(4*pi*x[1])'), degree=10)
-
-    def define_functions(self):
-        """
-        Define function to hold solution 
-        """
-        U = Function(self.space)
-        U = interpolate(self.ic, self.space)
-        return U
-
-
-class LidDrivenCavity():
+class LidDrivenCavity(Problem):
     """
     Analytical test case as described on page 390 in: 
     A. Logg et al. (2012)
@@ -171,21 +178,13 @@ class LidDrivenCavity():
     Springer-Verlag Berlin Heidelberg
     """
     def __init__(self, nx, ny):
-        self.mesh = self.make_mesh(nx, ny)
-        self.space = self.make_space()
-        self.bc = self.boundary_conditions()
-        self.ic = self.initial_conditions()
-        self.f = self.external_forces()
-        self.name = 'lid'
+        super().__init__(nx, ny, 'lid')
 
     def make_mesh(self, nx, ny): 
-        return UnitSquareMesh(nx, ny)
+        return super().make_mesh(nx, ny)
 
     def make_space(self):
-        scalar_element = FiniteElement('P', triangle, 1)
-        vector_element = VectorElement('P', triangle, 2)
-        mixed_element = MixedElement([scalar_element, vector_element])
-        return FunctionSpace(self.mesh, MixedElement([scalar_element, vector_element]))
+        return super().make_space()
 
     def boundary_conditions(self):
         pin = CompiledSubDomain('near(x[0], 0.0)  && on_boundary')
@@ -195,24 +194,70 @@ class LidDrivenCavity():
         right = CompiledSubDomain('near(x[0], 1.0) && on_boundary')
         bottom = CompiledSubDomain('near(x[1], 0.0) && on_boundary')
         top = CompiledSubDomain('near(x[1], 1.0) && on_boundary')
+        bcs = [left, right, bottom, top]
 
         u_wall = Constant((0, 0))
         u_lid = Expression(('x[0]*(1.0-x[0])', '0.0'), degree=2)
+        vels = [u_wall, u_wall, u_wall, u_lid]
 
-        u_bc1 = DirichletBC(self.space.sub(1), u_lid, top)
-        u_bc2 = DirichletBC(self.space.sub(1), u_wall, bottom)
-        u_bc3 = DirichletBC(self.space.sub(1), u_wall, left)
-        u_bc4 = DirichletBC(self.space.sub(1), u_wall, right)
-        return [u_bc1, u_bc2, u_bc3, u_bc4]
-
-    def initial_conditions(self):
-        return Constant((0, 0, 0))
+        return [DirichletBC(self.space.sub(1), vel, bc) for (vel, bc) in zip(vels, bcs)]
 
     def external_forces(self):
-        return Constant((-1.0, 1.0))
+        return super().external_forces()
+
+
+class Sinflow3D():
+    """
+    3D test case with a sinusoidal inflow as described in: 
+    https://fenicsproject.org/olddocs/dolfin/1.5.0/python/demo/documented/stokes-iterative/python/documentation.html
+    """
+    def __init__(self, nx, ny, nz):
+        self.mesh = self.make_mesh(nx, ny, nz)
+        self.space = self.make_space()
+        self.bc = self.boundary_conditions()
+        self.f = self.external_forces()
+        self.name = 'sinflow'
+
+    def make_mesh(self, nx, ny, nz): 
+        return UnitCubeMesh(nx, ny, nz)
+
+    def make_space(self):
+        scalar_element = FiniteElement('Lagrange', tetrahedron, 1)
+        vector_element = VectorElement('Lagrange', tetrahedron, 2)
+        mixed_element = MixedElement([scalar_element, vector_element])
+        return FunctionSpace(self.mesh, mixed_element)
+
+    def boundary_conditions(self):
+        # Boundaries
+        def right(x, on_boundary): return x[0] > (1.0 - DOLFIN_EPS)
+        def left(x, on_boundary): return x[0] < DOLFIN_EPS
+        def top_bottom(x, on_boundary): return x[1] > 1.0 - DOLFIN_EPS or x[1] < DOLFIN_EPS
+
+        # No-slip boundary condition for velocity
+        noslip = Constant((0.0, 0.0, 0.0))
+        bc0 = DirichletBC(self.space.sub(1), noslip, top_bottom)
+
+        # Inflow boundary condition for velocity
+        inflow = Expression(("-sin(x[1]*pi)", "0.0", "0.0"), degree=10)
+        bc1 = DirichletBC(self.space.sub(1), inflow, right)
+
+        # Boundary condition for pressure at outflow
+        bc2 = DirichletBC(self.space.sub(0), Constant(0), left)
+
+        # Collect boundary conditions
+        return [bc0, bc1, bc2]
+
+    def write_file(self, U, n, outdir='./output/'):
+        p, u = U.split(deepcopy=True)
+        file_p = File(outdir + self.name + '_n' + str(n) + '_' + 'pressure.pvd')
+        file_u = File(outdir + self.name + '_n' + str(n) + '_' + 'velocity.pvd')
+        file_p << p
+        file_u << u
+
+    def external_forces(self):
+        return Constant((0, 0, 0))
 
     def define_functions(self):
         # Define functions for solutions at previous and current time steps
         U = Function(self.space)
-        U = interpolate(self.ic, self.space)
         return U
