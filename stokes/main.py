6aec6cb59787ce762fb95e07213cda7c839f0329
knutsvk <ksk38@cam.ac.uk>
2017-05-18 15:49:31 +0100

    made functions and if __name__ == main bit to sort out what is being run

--- stokes/main.py
+++ stokes/main.py
@@ -1,46 +1,83 @@
 import fenics
 import pde
-from settings import *
 import testcases
 
 fenics.set_log_level(fenics.ERROR)
 
-
-# Smooth test case with linear analytical solution
-for nx, ny in [(3,3), (5,3), (3,5), (20,20), (50,50)]:
-    problem = testcases.Smooth(nx, ny)
-    algorithm = pde.GSP(problem, alpha, nu)
+def run_direct_polka(n=32):
+    problem = testcases.PolkaDots(n, n)
+    algorithm = pde.direct(problem, 0, 1)
     U = problem.define_functions()
     algorithm.solve(U)
+    problem.write_file(U, n, n)
     error_norms = problem.compute_errors(U)
-    assert max(max(error_norms)) < 1e-10
+    print(error_norms)
 
-# Polka dot pattern, sinusoidal analytical solution
-n = 128
-problem = testcases.PolkaDots(nx, ny)
-algorithm = pde.GSP(problem, alpha, nu)
-U = problem.define_functions()
-algorithm.solve(U)
+def run_iterative_polka(n=32):
+    problem = testcases.PolkaDots(n, n)
+    algorithm = pde.iterative(problem, 0, 1)
+    U = problem.define_functions()
+    algorithm.solve(U)
+    problem.write_file(U, n, n)
+    error_norms = problem.compute_errors(U)
+    print(error_norms)
 
-error_norms = problem.compute_errors(U)
-print(error_norms)
+def run_direct_lid(n=128):
+    problem = testcases.LidDrivenCavity(n, n)
+    algorithm = pde.direct(problem, 0, 1)
+    U = problem.define_functions()
+    algorithm.solve(U)
+    problem.write_file(U, n, n)
 
-p, u = U.split(deepcopy=True)
-outdir = './output/'
-file_p = fenics.File(outdir + problem.name + '_' + 'pressure.pvd')
-file_p << p
-file_u = fenics.File(outdir + problem.name + '_' + 'velocity.pvd')
-file_u << u
+def run_iterative_lid(n=128):
+    problem = testcases.LidDrivenCavity(n, n)
+    algorithm = pde.iterative(problem, 0, 1)
+    U = problem.define_functions()
+    algorithm.solve(U)
+    problem.write_file(U, n, n)
+
+def run_direct_3d(n=8):
+    problem = testcases.Sinflow3D(n, n, n)
+    algorithm = pde.direct(problem, 0, 1)
+    U = problem.define_functions()
+    algorithm.solve(U)
+    problem.write_file(U, n, n)
+
+def run_iterative_3d(n=8):
+    problem = testcases.Sinflow3D(n, n, n)
+    algorithm = pde.iterative(problem, 0, 1)
+    U = problem.define_functions()
+    algorithm.solve(U)
+    problem.write_file(U, n, n)
+
+def test_direct_smooth():
+    print('Running test_direct_smooth()')
+    for nx, ny in [(3,3), (5,3), (3,5), (20,20)]:
+        for nu in [0.01, 1, 100]: 
+            print('%d x %d mesh, nu = %.2f' % (nx, ny, nu))
+            problem = testcases.Smooth(nx, ny)
+            algorithm = pde.direct(problem, 0, nu)
+            U = problem.define_functions()
+            algorithm.solve(U)
+            error_norms = problem.compute_errors(U)
+            largest_error_norm = max(max(error_norms))
+            assert largest_error_norm < 1e-6, 'Largest error norm = %g' % largest_error_norm
+    print('Done!')
+
+def test_direct_polka():
+    print('Running test_direct_polka()')
+    for nx, ny in [(32,32), (64,64), (128,128)]:
+        for nu in [0.01, 1, 100]: 
+            print('%d x %d mesh, nu = %.2f' % (nx, ny, nu))
+            problem = testcases.PolkaDots(nx, ny)
+            algorithm = pde.direct(problem, 0, 1)
+            U = problem.define_functions()
+            algorithm.solve(U)
+            error_norms = problem.compute_errors(U)
+            largest_error_norm = max(max(error_norms))
+            assert largest_error_norm < 1, 'Largest error norm = %g' % largest_error_norm
+    print('Done!')
 
 
-# Regularised lid-driven cavity 
-problem = testcases.LidDrivenCavity(nx, ny)
-algorithm = pde.GSP(problem, alpha, nu)
-U = problem.define_functions()
-algorithm.solve(U)
-
-p, u = U.split(deepcopy=True)
-file_p = fenics.File(outdir + problem.name + '_' + 'pressure.pvd')
-file_p << p
-file_u = fenics.File(outdir + problem.name + '_' + 'velocity.pvd')
-file_u << u
+if __name__ == '__main__':
+    run_direct_lid(64)
