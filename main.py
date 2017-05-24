8854749ba0aea3b9d3ebc25f2992eb78b539ecf5
knutsvk <ksk38@cam.ac.uk>
2017-05-24 12:26:09 +0100

    tried to make better module layout

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
 import fenics
-import pde
-import testcases
+import stokes
+import problems
 
 fenics.set_log_level(fenics.ERROR)
 
-def run_direct_polka(n=32):
-    problem = testcases.PolkaDots(n, n)
-    algorithm = pde.direct(problem, 0, 1)
-    U = problem.define_functions()
-    algorithm.solve(U)
-    problem.write_file(U, n, n)
-    error_norms = problem.compute_errors(U)
-    print(error_norms)
-
-def run_iterative_polka(n=32):
-    problem = testcases.PolkaDots(n, n)
-    algorithm = pde.iterative(problem, 0, 1)
+def run_direct_polka(n=128):
+    problem = problems.PolkaDots(n, n)
+    algorithm = stokes.direct(problem, 0, 1)
     U = problem.define_functions()
     algorithm.solve(U)
     problem.write_file(U, n, n)
@@ -23,15 +14,8 @@ def run_iterative_polka(n=32):
     print(error_norms)
 
 def run_direct_lid(n=128):
-    problem = testcases.LidDrivenCavity(n, n)
-    algorithm = pde.direct(problem, 0, 1)
-    U = problem.define_functions()
-    algorithm.solve(U)
-    problem.write_file(U, n, n)
-
-def run_iterative_lid(n=128):
-    problem = testcases.LidDrivenCavity(n, n)
-    algorithm = pde.iterative(problem, 0, 1)
+    problem = problems.LidDrivenCavity(n, n)
+    algorithm = stokes.direct(problem, 0, 1)
     U = problem.define_functions()
     algorithm.solve(U)
     problem.write_file(U, n, n)
@@ -41,8 +25,8 @@ def test_direct_smooth():
     for nx, ny in [(3,3), (5,3), (3,5), (20,20)]:
         for nu in [0.01, 1, 100]: 
             print('%d x %d mesh, nu = %.2f' % (nx, ny, nu))
-            problem = testcases.Smooth(nx, ny)
-            algorithm = pde.direct(problem, 0, nu)
+            problem = problems.Smooth(nx, ny)
+            algorithm = stokes.direct(problem, 0, nu)
             U = problem.define_functions()
             algorithm.solve(U)
             error_norms = problem.compute_errors(U)
@@ -52,11 +36,11 @@ def test_direct_smooth():
 
 def test_direct_polka():
     print('Running test_direct_polka()')
-    for nx, ny in [(32,32), (64,64), (128,128)]:
+    for nx, ny in [(32,32), (64,64)]:
         for nu in [0.01, 1, 100]: 
             print('%d x %d mesh, nu = %.2f' % (nx, ny, nu))
-            problem = testcases.PolkaDots(nx, ny)
-            algorithm = pde.direct(problem, 0, 1)
+            problem = problems.PolkaDots(nx, ny)
+            algorithm = stokes.direct(problem, 0, 1)
             U = problem.define_functions()
             algorithm.solve(U)
             error_norms = problem.compute_errors(U)
@@ -66,4 +50,5 @@ def test_direct_polka():
 
 
 if __name__ == '__main__':
-    run_direct_lid(64)
+    test_direct_smooth()
+    test_direct_polka()
